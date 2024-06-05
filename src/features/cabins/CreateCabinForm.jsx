d8089556ff6b531f5ad0f86b5a46935ae01f561e
Olga Tyumeneva <pinkangel5@ya.ru>
2024-06-05 15:16:43 +0300

    feat(cabins): add to upload image to supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,5 +1,3 @@
-import styled from "styled-components";
-
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
 import Button from "../../ui/Button";
@@ -13,42 +11,6 @@ import {
 import { createCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 import FormRow from "../../ui/FormRow";
-
-const FormRow2 = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
 
 function CreateCabinForm() {
   const {
@@ -76,7 +38,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -147,7 +109,8 @@ function CreateCabinForm() {
           {...register("discount", {
             required: "This field is required",
             validate: (value) =>
-              value < getValues().regularPrice ||
+              Number(value) <
+                Number(getValues().regularPrice) ||
               "Discount should be less than regular price",
           })}
         />
@@ -172,7 +135,9 @@ function CreateCabinForm() {
         <FileInput
           id="image"
           accept="image/*"
-          disabled={isCreating}
+          {...register("image", {
+            required: "This field is required",
+          })}
         />
       </FormRow>
 
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
+export const supabaseUrl =
   "https://wmifkakwcauzoioiizku.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtaWZrYWt3Y2F1em9pb2lpemt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0NTYxMjEsImV4cCI6MjAzMzAzMjEyMX0.TuBb6Byob5eJITehHkbQkN42Z1AwXKw7XSSguCR3k7M";
