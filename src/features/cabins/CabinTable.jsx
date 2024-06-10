0743bd76e0a1c7111a4d578fef92a5462bfcb1da
Olga Tyumeneva <pinkangel5@ya.ru>
2024-06-11 01:12:53 +0300

    feat(cabins): add sorting

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -10,7 +10,7 @@ function CabinTable() {
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
-
+  // 1. FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
@@ -27,6 +27,17 @@ function CabinTable() {
       (cabin) => cabin.discount > 0
     );
 
+  // 2. SORT
+  const sortBy =
+    searchParams.get("sortBy") || "startDate-asc";
+
+  const [field, directions] = sortBy.split("-");
+  const modifier = directions === "asc" ? 1 : -1;
+
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -39,7 +50,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => (
             <CabinRow cabin={cabin} key={cabin.id} />
           )}
