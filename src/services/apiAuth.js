4156b5ebca6d5dcda235a3371690a6016ab03014
Olga Tyumeneva <pinkangel5@ya.ru>
2024-06-13 18:01:00 +0300

    feat: add authentication

---
+++ src/services/apiAuth.js
@@ -0,0 +1,15 @@
+import supabase from "./supabase";
+
+export async function login({ email, password }) {
+  const { data, error } =
+    await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
